92b8bb7c8b8c2d03249119f71b99c3310ebcf472
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-07-15 10:16:36 +0300

    add 007

---
+++ react-practice-projects/007-weather-app/src/Api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e36467e9b6msh9c6710b49977ee3p19d968jsnfcd82c0e232c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4afb9ebf7236815089562a7e66026802";
